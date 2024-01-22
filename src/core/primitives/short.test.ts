578b0a15a1f5eee23dfcfd64aa79c927eebb4a6f
Jonathan Rayback <jrayback@gmail.com>
2024-01-22 13:49:45 -0700

    clean up from last commit

---
+++ src/core/primitives/short.test.ts
@@ -0,0 +1,12 @@
+import { buildShort } from './short'
+
+describe('Build Primitives', () => {
+  describe('One-character Codes', () => {
+    test('Short Number', () => {
+      const short = 67
+      const expected = 'AAAA'
+      const actual = buildShort(short)
+      expect(expected).toBe(actual)
+    })
+  })
+})
--- src/lib/util.ts
+++
@@ -1,3 +0,0 @@
-export const calculatePadSize = (bytes: number): number => {
-  return (3 - (bytes % 3)) % 3
-}
