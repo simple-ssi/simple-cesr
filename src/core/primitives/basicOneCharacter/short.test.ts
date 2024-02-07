01df5ce6aaa40e594c927c13c04bdae9dcd0d446
Jonathan Rayback <jrayback@gmail.com>
2024-02-07 11:51:02 -0700

    add tests for Short

---
+++ src/core/primitives/basicOneCharacter/short.test.ts
@@ -0,0 +1,10 @@
+import { makeShort } from './short'
+
+test('makeShort', () => {
+  expect(makeShort(0).value.toString('hex')).toBe('0000')
+  expect(makeShort(1).value.toString('hex')).toBe('0001')
+  expect(makeShort(255).value.toString('hex')).toBe('00ff')
+  expect(makeShort(256).value.toString('hex')).toBe('0100')
+  expect(makeShort(65535).value.toString('hex')).toBe('ffff')
+  expect(() => makeShort(65536)).toThrow('primitive is too big')
+})
