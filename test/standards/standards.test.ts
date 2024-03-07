6307397921e4369cfa13ff8c0f31ef56e2698075
Jonathan Rayback <jrayback@gmail.com>
2024-03-06 19:26:05 -0700

    finalize standards test

--- test/standards/standards.test.ts
+++ test/standards/standards.test.ts
@@ -1,21 +1,31 @@
+import { Buffer } from 'buffer'
 import { standards } from './standards.ts'
 import { encodeAsText } from '../../src/api/encode/encodeAsText.ts'
 import { Code } from '../../src/core/code/code.ts'
 
 const matter = standards.matter
 
-describe('Standards', () => {
-  // matter.forEach((pad) => {
-  //   pad.examples.forEach((example) => {
-  //     example.values.forEach((value) => {
-  //       it(`should encode ${value.raw}... as ${value.text}`, () => {
-  //         expect(encodeAsText(example.code as Code, value.raw)).toBe(value.text)
-  //       })
-  //     })
-  //   })
-  // })
-  // true should be true
-  it('should be true', () => {
-    expect(true).toBe(true)
+// to dedent the string literal
+const shouldEncodeRawAsText = (raw: string, text: string): string => {
+  return `should encode 
+    ${raw}
+    as 
+    ${text}`
+}
+
+describe('Standards: Text Encoder', () => {
+  matter.forEach((pad) => {
+    pad.examples.forEach((example) => {
+      example.values.forEach((value) => {
+        it(shouldEncodeRawAsText(value.raw, value.text), () => {
+          const binary = Buffer.from(value.raw, 'hex')
+          expect(encodeAsText(example.code as Code, binary)).toBe(value.text)
+        })
+      })
+    })
   })
+  // true should be true
+  // it('should be true', () => {
+  //   expect(true).toBe(true)
+  // })
 })
