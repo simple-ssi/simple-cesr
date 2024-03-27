afcd213486fda032dfcedeefb8f1c9402ae98fc0
Jonathan Rayback <jrayback@gmail.com>
2024-03-27 17:59:31 -0600

    finalize build script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('Welcome to simple-cesr!', 'Stay tuned for an amazing demo...still to come!')
+import { encodeText } from './api/encode/encoders/encodeText.js'
+
+console.log('Welcome to simple-cesr!')
+console.log("We'll be doing amazing things like this:")
+
+const primitive = new Uint8Array([0x00, 0x01]) // two bytes
+console.log('Primitive:', primitive)
+const encoded = encodeText('M', primitive)
+console.log('Encoded:', encoded)
